ba1feef90aebb73649b1967f745858ce72ddfd38
David H. Friedman <david.h.friedman.nyc@gmail.com>
2019-01-11 07:46:12 -0500

    single step

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const SIDE = 20
 
-const now = new Array(SIDE*SIDE);
-const next = new Array(SIDE*SIDE);
+/* screen N x N
+   now and next are N+1 x N+1 arrays with extra "dead" rows at top and bottom, columns an left and right
+   eliminate edge cases when counting neighbors
+   this makes now and next 1 based arrays!
+   screen(row, col) is now[ (SIDE+2)*(row+1) + col) + 1 ]
+*/
+
+const now = new Array((SIDE+2)*(SIDE+2));
+const next = new Array((SIDE+2)*(SIDE+2));
+
+/* (row,col) to index */
+function rc_to_i(r, c) {
+  return ((SIDE+2)*(r+1) + c) + 1;
+}
 
 now.fill(0);
 next.fill(0);
@@ -36,7 +48,7 @@ function copyScreenTo(where) {
   let cells = [...document.querySelectorAll(".cell")];
   for(let row = 0; row < SIDE; row++) {
     for(let col = 0; col < SIDE; col++) {
-        where[SIDE*row + col] = cells[SIDE*row + col].classList.contains("live") ? 1 : 0;
+        where[rc_to_i(row, col)] = cells[SIDE*row + col].classList.contains("live") ? 1 : 0;
     }
   }
 }
@@ -45,22 +57,42 @@ function copyToScreen(whence) {
   let cells = [...document.querySelectorAll(".cell")];
   for(let row = 0; row < SIDE; row++) {
     for(let col = 0; col < SIDE; col++) {
-        cells[SIDE*row + col].classList.toggle("live", whence[SIDE*row + col] === 1);
+        cells[SIDE*row + col].classList.toggle("live", whence[rc_to_i(row, col)] === 1);
     }
   }
 }
 
 function run() {
   copyScreenTo(now);
+  beFruitfulAndMultiply(now, next);
+  copyToScreen(next);
+  let t = now;
+  now = next;
+  next = now;
 }
 
 function randomize() {
   for(let row = 0; row < SIDE; row++) {
     for(let col = 0; col < SIDE; col++) {
-        now[SIDE*row + col] = Math.random() < .5 ? 1 : 0;
+        now[rc_to_i(row, col)] = Math.random() < .5 ? 1 : 0;
     }
   }
   copyToScreen(now);
+}
+
+function countNeighbors(a, r, c) {
+  return a[rc_to_i(r-1, c-1)] + a[rc_to_i(r-1, c)]   + a[rc_to_i(r-1, c+1)] +
+         a[rc_to_i(r,   c-1)] +                        a[rc_to_i(r,   c+1)] +
+         a[rc_to_i(r+1, c-1)] + a[rc_to_i(r+1, c)]   + a[rc_to_i(r+1, c+1)];
+}
+
+function beFruitfulAndMultiply(now, next) {
+  for(let row = 0; row < SIDE; row++) {
+    for(let col = 0; col < SIDE; col++) {
+      let neighbors = countNeighbors(now, row, col);
+      next[rc_to_i(row, col)] = (neighbors < 2 || neighbors > 3) ? 0 : 1;
+    }
+  }
 }
 
 /*
